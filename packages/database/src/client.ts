62f8c03a02939b49b10a85560657d3bb2fd04322
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2023-01-29 21:46:32 +0100

    add eslint

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-export * from "@prisma/client";
+import { PrismaClient } from ".prisma/client";
+export * from ".prisma/client";
 
 declare global {
   // eslint-disable-next-line no-var
